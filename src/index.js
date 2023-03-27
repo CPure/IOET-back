6bea31dc08ffcc530eca28dce6cc8b6c62a2d9d2
Cpure <carlospures@gmail.com>
2023-03-26 21:51:40 -0500

    prettier.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
 const http = require('http')
 const { salary } = require('./controllers/salary')
 
-const server = http.createServer(async (req, res)=>{
+const server = http.createServer(async (req, res) => {
     const { url, method } = req
 
     console.log(`URL: ${url} - Method: ${method}`)
-    try{
-        if(method === 'POST'){
+    try {
+        if (method === 'POST') {
             if (url === "/api/salary") {
                 salary(req, res)
             }
-            if(url !== "/api/salary"){
-                res.writeHead(404, 
-                    {   
-                        'Content-Type': 'application/json', 
-                        "Access-Control-Allow-Origin":"*", 
-                        "Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept",
-                         "Access-Control-Allow-Methods" : "OPTIONS, POST, GET"    }
-                    )
-                res.write(JSON.stringify({message : 'Does not exist'}))
+            if (url !== "/api/salary") {
+                res.writeHead(404,
+                    {
+                        'Content-Type': 'application/json',
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+                        "Access-Control-Allow-Methods": "OPTIONS, POST, GET"
+                    }
+                )
+                res.write(JSON.stringify({ message: 'Does not exist' }))
                 res.end()
             }
         }
-        if(method !== 'POST'){
-            res.writeHead(200, {'Content-Type': 'application/json', "Access-Control-Allow-Origin":"*", // REQUIRED CORS HEADER
-			"Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept" })
-            res.write(JSON.stringify({message : 'Hello'}))
+        if (method !== 'POST') {
+            res.writeHead(200, {
+                'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*", // REQUIRED CORS HEADER
+                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+            })
+            res.write(JSON.stringify({ message: 'Hello' }))
             res.end()
         }
-    }catch(e){
+    } catch (e) {
         console.log(e)
     }
 
-   
+
 })
 
 server.listen(3100)
